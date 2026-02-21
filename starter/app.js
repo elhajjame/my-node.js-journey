2d5faed6e04bf6418690409763b52a3056a8ffaf
mehdi el-hajjame <el-hajjame@outlook.com>
2026-02-21 21:57:47 +0000

    parsing url

--- starter/app.js
+++ starter/app.js
@@ -37,6 +37,7 @@ const url = require('url');
 //============= SERVER ================
 //
 const replaceTemplate = (temp, product) => {
+  // g: global search → replace ALL matches
   let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
   output = output.replace(/{%IMAGE%}/g, product.image);
   output = output.replace(/{%PRICE%}/g, product.price);
@@ -49,37 +50,43 @@ const replaceTemplate = (temp, product) => {
   if (!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
   return output;
 }
+//__dirname = the absolute path of the current file’s folder
 const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
-const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-cards.html`, 'utf-8');
 const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
-
+// console.log(tempProduct);
 // Reading the file inside the request handler is inefficient
 // because the file will be read from disk on every request.
 // Since this data is static, it's better to read it once when
 // the server starts using readFileSync and reuse it.
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
-
 // first we create the server and second we start the server
 const server = http.createServer((req, res) => {
   // this the res that we going to send
   // each time we hit the server this function we will called and the callback will have access
   // ==> so here is sending back a simple response to the clint 
-  const pathName = req.url;
+  const { query, pathname } = url.parse(req.url, true)
+  console.log(req.url);
+  console.log(url.parse(req.url, true));
   //------- overview ---------
-  if (pathName === '/' || pathName === '/overview') {
+  if (pathname === '/' || pathname === '/overview') {
     // the map method it's accept a callback and applies that function to each element of an array,then return a new array
-    res.writeHead(200, { 'content-type': 'text/html' })
-    const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el));
-    console.log(cardsHtml);
-    res.end(tempOverview);
+    res.writeHead(200, { 'content-type': 'text/html' });
+    const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
+    const output = tempOverview.replace('{%PRODUCT_CARDS%}', cardsHtml)
+    // console.log(cardsHtml);
+    res.end(output);
 
-    //------- product ---------
-  } else if (pathName === '/product') {
-    res.end('this is the PRODUCT page');
+    //------- product page ---------
+  } else if (pathname === '/product') {
+    res.writeHead(200, { "content-type": "text/html" });
+    const product = dataObj[query.id];
+    const output = replaceTemplate(tempProduct, product);
+    res.end(output);
 
     //------- error page ---------
-  } else if (pathName === '/api') {
+  } else if (pathname === '/api') {
     res.writeHead(200, { 'content-type': 'application/json' });
     // console.log(data);
     res.end(data);

70f21758ab003edeaa29f040811c9f5c7bb5ac04
mehdi el-hajjame <el-hajjame@outlook.com>
2026-02-21 23:38:46 +0000

    i have leared about npm and type of packages and how to install them

--- starter/app.js
+++ starter/app.js
@@ -2,6 +2,8 @@
 const fs = require('fs');
 const http = require('http');
 const url = require('url');
+
+const replaceTemplate = require('./module/replaceTemplate');
 // const replaceTemplate = require('../final/modules/replaceTemplate');
 ///////////////////////////
 // =========== files =============
@@ -36,20 +38,7 @@ const url = require('url');
 ///////////////////////////////////////////////////////////////
 //============= SERVER ================
 //
-const replaceTemplate = (temp, product) => {
-  // g: global search → replace ALL matches
-  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
-  output = output.replace(/{%IMAGE%}/g, product.image);
-  output = output.replace(/{%PRICE%}/g, product.price);
-  output = output.replace(/{%FROM%}/g, product.from);
-  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-  output = output.replace(/{%QUANTITY%}/g, product.quantity);
-  output = output.replace(/{%DESCRIPTION%}/g, product.description);
-  output = output.replace(/{%ID%}/g, product.id);
 
-  if (!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
-  return output;
-}
 //__dirname = the absolute path of the current file’s folder
 const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
 const tempCard = fs.readFileSync(`${__dirname}/templates/template-cards.html`, 'utf-8');
---
+++ starter/module/replaceTemplate.js
@@ -0,0 +1,17 @@
+module.exports = (temp, product) => {
+  // g: global search → replace ALL matches
+  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%ID%}/g, product.id);
+
+  if (!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+  return output;
+}
+//----------- NOTES -------------
+// Node.js treats each file in a Node project as a module that can export values and functions from the file.
+//
